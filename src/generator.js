374741c301506a738634540e88e55b743c0d489d
Garet McKinley <self@mediachicken.com>
2016-11-05 00:49:30 -0400

    initial commit

---
+++ src/generator.js
@@ -0,0 +1,28 @@
+import { css } from 'styled-components';
+import { gradients, randomGradientName } from './gradients';
+
+
+function generator(props = {}) {
+  let gradient = '';
+  let angle = -90;
+  if (props.gradient === undefined) {
+    gradient = randomGradientName();
+  } else {
+    gradient = props.gradient;
+  }
+  if (props.angle !== undefined) {
+    angle = props.angle;
+  }
+  return css`background-color: ${gradients[gradient][0]};
+background-image: -webkit-linear-gradient(
+  ${angle}deg,
+  ${gradients[gradient][0]},
+  ${gradients[gradient][1]});
+background-image: linear-gradient(
+  ${angle}deg,
+  ${gradients[gradient][0]},
+  ${gradients[gradient][1]});`;
+}
+
+export default generator;
+
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import generator from './generator';
+import Gradient from './components/gradient';
+
+export { generator, Gradient };
+

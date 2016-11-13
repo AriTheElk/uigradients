f85ad03c8fb81234daba59b2427c98a85b6cf4ec
Garet McKinley <self@mediachicken.com>
2016-11-13 16:35:23 -0500

    Added storybook support
    Added support for automatically generating storybooks.
    These are used for previewing the current selection of
    gradients in a sane and user-friendly manner.

---
+++ stories/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+import styled from 'styled-components';
+import { gradients } from '../src/gradients.js';
+import Gradient from '../src/components/gradient.js';
+
+const Preview = styled(Gradient)`
+  border-radius: 4px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, .1);
+  color: #fff;
+  font-family: sans-serif;
+  margin-bottom: 10px;
+  padding: 50px;
+  text-align: center;
+  text-shadow: 0 1px 4px rgba(0, 0, 0, .6);
+`;
+
+const map = new Map(Object.entries(gradients));
+
+const GradientPreviews = [];
+
+map.forEach(function (value, key) {
+  GradientPreviews.push(key);
+});
+
+storiesOf('Gradient Component', module)
+  .add('Color Previews', () => (
+    <div>
+      {GradientPreviews.map(function (value) {
+        return (<Preview>{value}</Preview>)
+      })}      
+    </div>
+  ));
+

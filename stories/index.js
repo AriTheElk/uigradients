f66347ba0a5e6a4cfa7e9f87789919b26f244f5e
Garet McKinley <self@garet.io>
2019-10-24 00:29:11 -0400

    add storybook knobs

--- stories/index.js
+++ stories/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+
 import styled from "styled-components";
 import { gradients } from "../src/gradients.js";
 import Gradient from "../src/components/gradient.js";
+
+const stories = storiesOf("Gradient Component", module);
+stories.addDecorator(withKnobs);
 
 const Preview = styled(Gradient)`
   border-radius: 4px;
@@ -23,26 +29,30 @@ map.forEach(function(value, key) {
   GradientPreviews.push(key);
 });
 
-storiesOf("Gradient Component", module)
-  .add("Linear Gradient Previews", () => (
-    <div>
-      {GradientPreviews.map(function(value, id) {
-        return (
-          <Preview gradient={value} key={id}>
-            {value}
-          </Preview>
-        );
-      })}
-    </div>
-  ))
-  .add("Radial Gradient Previews", () => (
-    <div>
-      {GradientPreviews.map(function(value, id) {
-        return (
-          <Preview gradient={value} key={id} type={"radial"}>
-            {value}
-          </Preview>
-        );
-      })}
-    </div>
-  ));
+stories.add("Gradient Presets", () => (
+  <div>
+    {GradientPreviews.map(function(value, id) {
+      return (
+        <Preview
+          gradient={value}
+          key={id}
+          type={boolean("Radial", false) ? "radial" : "linear"}
+        >
+          {value}
+        </Preview>
+      );
+    })}
+  </div>
+));
+
+stories.add("Custom Gradients", () => (
+  <div>
+    {GradientPreviews.map(function(value, id) {
+      return (
+        <Preview gradient={value} key={id} type={"radial"}>
+          {text("Label", "Hello Storybook")}
+        </Preview>
+      );
+    })}
+  </div>
+));
